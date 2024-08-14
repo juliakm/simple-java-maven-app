6e2d4045c07878f80d274a4c8f58d9730d502bf9
Julia Kulla-Mader <64906+juliakm@users.noreply.github.com>
2024-08-14 16:07:04 -0400

    fix format

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,24 +1,18 @@
 package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-{
     @Test
     public void testAppConstructor() {
-        App app1 = new App();
-        App app2 = new App();
-        assertEquals(app1.getMessage(), app2.getMessage());
+        App app = new App();
+        assertNotNull(app);
     }
 
     @Test
-    public void testAppMessage()
-    {
+    public void testAppMessage() {
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
     }
